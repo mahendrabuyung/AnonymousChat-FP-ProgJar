3fa0d262d8ff8097db48e17dc26324044b34333c
Rahma sofyantoro <rahma.sofyantoro16@mhs.if.its.ac.id>
2019-05-23 22:51:31 +0700

    Emo Add

---
+++ ClientApp/Emo Window (2).py
@@ -0,0 +1,10 @@
+label = [
+":grinning_face:",
+":grinning_face_with_smiling_eyes:",
+":grinning_face_with_big_eyes:",
+":grinning_face_with_big_eyes:",
+":grinning_face:",
+":grinning_face_with_smiling_eyes:",
+":grinning_face_with_big_eyes:",
+":grinning_face_with_big_eyes:"
+]
